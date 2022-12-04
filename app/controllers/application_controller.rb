a191eef0a1508a7adcc47ab04a09d61d487aeae0
Echesa Imaculate Zepwa <imaculate.zepwa@student.moringaschool.com>
2022-12-04 16:53:08 +0300

    complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+  rescue_from ActiveRecord::RecordInvalid, with: :handle_invalid
+  before_action :authorize
 
-end
+
+  private
+
+  def authorize
+    @current_user = User.find_by(id: session[:user_id])
+    return render json: {errors: ["Not authorized"]}, status: :unauthorized unless @current_user
+  end
