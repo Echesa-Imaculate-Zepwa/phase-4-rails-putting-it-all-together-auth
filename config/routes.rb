81e6be69957741aaee4c4e5944350f32b844d7b1
Echesa Imaculate Zepwa <imaculate.zepwa@student.moringaschool.com>
2022-12-05 18:19:27 +0300

    complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-  resources :recipes, only: [:index, :create]
-  post "/login", to: "sessions#create"
-  post "/signup", to: "users#create"
-  get "/me", to: "users#show"
-  delete "/logout", to: "sessions#destroy"
-
+  
+  Rails.application.routes.draw do
+    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    resources :recipes, only: [:index, :create]
+    post "/signup", to: "users#create"
+    get "/me", to: "users#show"
+    post "/login", to: "sessions#create"
+    delete "/logout", to: "sessions#destroy"
+  end
   #resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
